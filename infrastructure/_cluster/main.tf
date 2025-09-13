70b129c4c5c6f204bb1a0852337004f03494d7b7
IsrealAde <isrealade6@gmail.com>
2025-09-13 22:47:32 +0100

    updated

--- infrastructure/_cluster/main.tf
+++ infrastructure/_cluster/main.tf
@@ -55,6 +55,8 @@ module "eks" {
     support_type = "STANDARD"
   }
 
+  create_cloudwatch_log_group = false
+
   vpc_id     = module.vpc.vpc_id
   subnet_ids = concat(module.vpc.public_subnet_ids, module.vpc.private_subnet_ids)
 
